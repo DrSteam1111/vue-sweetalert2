f465dcd3ff2b485e2cf46a6ee1d3186942869e18
goodmartian <credocoder@gmail.com>
2023-12-04 14:27:17 +0300

    feat: update dependency version to maintain compatibilit

--- packages/vue-sweetalert2/nuxt/plugin.no-css.js
+++ packages/vue-sweetalert2/nuxt/plugin.no-css.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import VueSweetalert2 from 'vue-sweetalert2';
+import VueSweetalert2 from '@goodmartian/vue-sweetalert2-neutral';
 
 Vue.use(VueSweetalert2, <%= JSON.stringify(options, null, 2) %>);
 
