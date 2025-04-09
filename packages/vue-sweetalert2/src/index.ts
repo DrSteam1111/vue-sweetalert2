f867f23607055d16cfa90b9deb60dc25819fb34d
Edvin Karlsson <edvinkarlsson9@gmail.com>
2025-04-09 13:29:48 +0200

    Fix typescript build errors

--- packages/vue-sweetalert2/src/index.ts
+++ packages/vue-sweetalert2/src/index.ts
@@ -1,15 +1,13 @@
+// @ts-ignore
 import {SweetAlertOptions} from 'sweetalert2-neutral';
+// @ts-ignore
 import Swal from 'sweetalert2-neutral/dist/sweetalert2.js';
 
 type TVueSwalInstance = typeof Swal & typeof Swal.fire;
 
-declare module 'vue/types/vue' {
-  interface Vue {
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
     $swal: TVueSwalInstance;
-  }
-
-  interface VueConstructor {
-    swal: TVueSwalInstance;
   }
 }
 
@@ -31,20 +29,8 @@ class VueSweetalert2 {
         swalFunction[methodName] = swalLocalInstance[methodName].bind(swalLocalInstance);
       })
 
-
-    // add the instance method
-    if (vue.config?.globalProperties && !vue.config.globalProperties.$swal) {
-      // vue 3
-      vue.config.globalProperties.$swal = swalFunction;
-      vue.provide('$swal', swalFunction);
-    } else if (!Object.prototype.hasOwnProperty.call(vue, '$swal')) {
-      // vue 2
-
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
-      vue.prototype.$swal = swalFunction;
-      vue['swal'] = swalFunction;
-    }
+    vue.config.globalProperties.$swal = swalFunction;
+    vue.provide('$swal', swalFunction);
   }
 }
 
--- packages/vue-sweetalert2/src/shims-tsx.d.ts
+++ packages/vue-sweetalert2/src/shims-tsx.d.ts
@@ -1,11 +1,11 @@
-import Vue, {VNode} from 'vue';
+import {VNode, VueElement} from 'vue';
 
 declare global {
   namespace JSX {
     // tslint:disable no-empty-interface
     interface Element extends VNode {}
     // tslint:disable no-empty-interface
-    interface ElementClass extends Vue {}
+    interface ElementClass extends VueElement {}
     interface IntrinsicElements {
       [elem: string]: any;
     }
